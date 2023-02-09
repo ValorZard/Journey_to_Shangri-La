0b86b0b01387899f44a9e1b200b0f7462df629da
Echolocati0n <esinkim917@gmail.com>
2023-02-09 02:02:48 -0800

    Stats System first build
    Title + some testing

---
+++ Echo/StatsFunc.gd
@@ -0,0 +1,26 @@
+extends Node
+
+class_name StatsFunc
+
+var kind : int
+var charm : int
+var brave : int
+var smart : int
+
+func _init(stats):
+	kind = stats.kind
+	charm = stats.charm
+	brave = stats.brave
+	smart = stats.smart
+	
+func change_kind(value):
+	kind += value
+	
+func change_charm(value):
+	charm += value
+
+func change_brave(value):
+	brave += value
+	
+func change_smart(value):
+	smart += value
