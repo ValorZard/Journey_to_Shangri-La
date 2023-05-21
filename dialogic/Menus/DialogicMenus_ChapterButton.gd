269136cd1c6f20aa12349fc1aebd2e1d79640291
danicaj3w <104111962+danicaj3w@users.noreply.github.com>
2023-05-20 22:56:48 -0700

    Small fixes to chapter selection and variables
    Fixed error with Raj Love Event 1 and added variables for love meters in dialogic

--- dialogic/Menus/DialogicMenus_ChapterButton.gd
+++ dialogic/Menus/DialogicMenus_ChapterButton.gd
@@ -16,13 +16,3 @@ func set_text(chapter_name : String) -> void:
 
 func _on_ChapButton_pressed():
 	emit_signal("pressed", $Label.text)
-
-# Hover animations
-#func _on_ChapButton_mouse_entered():
-#	print("Mouse Hovering")
-#	$Border/ColorRect.color = Color(126,220,230,255)
-
-
-#func _on_ChapButton_mouse_exited():
-#	print("Mouse Left")
-#	$Border/ColorRect.color = Color(126,220,230,0)
